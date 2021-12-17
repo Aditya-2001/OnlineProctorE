ea9bee87d6205309831bfe02d93be13eb209f872
Asinghkaushik <iib2019017@iiita.ac.in>
2021-12-17 18:24:06 +0530

    Faculty Dashboard Added

---
+++ Front-end/DashboardFaculty/DashboardFaculty/js/password.js
@@ -0,0 +1,5 @@
+if(document.body.clientWidth<992){
+    document.getElementById("profTemp").setAttribute("data-toggle","");
+    document.getElementById("profTemp").setAttribute("data-target","");
+    document.getElementById("profTemp").setAttribute("href","Profile.html");
+}
