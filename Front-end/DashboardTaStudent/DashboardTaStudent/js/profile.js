6b5e661d54c4e64efecdee74d5a3810082431b49
Asinghkaushik <iib2019017@iiita.ac.in>
2021-12-22 23:45:04 +0530

    Dashboard Page of TA and Student added

---
+++ Front-end/DashboardTaStudent/DashboardTaStudent/js/profile.js
@@ -0,0 +1,5 @@
+if(document.body.clientWidth<992){
+    document.getElementById("passTemp").setAttribute("data-toggle","");
+    document.getElementById("passTemp").setAttribute("data-target","");
+    document.getElementById("passTemp").setAttribute("href","Password.html");
+}
---
+++ Front-end/DashboardTaStudent/DashboardTaStudent/js/scripts.js
@@ -0,0 +1,14 @@
+if(document.body.clientWidth<992){
+    document.getElementById("profTemp").setAttribute("data-toggle","");
+    document.getElementById("profTemp").setAttribute("data-target","");
+    document.getElementById("passTemp").setAttribute("data-toggle","");
+    document.getElementById("passTemp").setAttribute("data-target","");
+    document.getElementById("profTemp").setAttribute("href","Profile.html");
+    document.getElementById("passTemp").setAttribute("href","Password.html");
+}
+
+$(document).ready(function () {
+    $('.dtBasicExample').DataTable();
+    $('.dtBasicExample1').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+});
