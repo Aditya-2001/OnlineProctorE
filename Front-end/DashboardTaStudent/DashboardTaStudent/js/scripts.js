859abfccaac2fd2d605ab25fc27b2c0f0bbd1d10
divy3011 <divyias3011@gmail.com>
2022-01-05 20:49:52 +0530

    view quiz page after quiz is submitted displayed

--- Front-end/DashboardTaStudent/DashboardTaStudent/js/scripts.js
+++ Front-end/DashboardTaStudent/DashboardTaStudent/js/scripts.js
@@ -8,7 +8,9 @@ if(document.body.clientWidth<992){
 }
 
 $(document).ready(function () {
-    $('.dtBasicExample').DataTable();
+    $('.dtBasicExample').DataTable({
+        "aaSorting": [[ 4, "desc" ], [ 5, "desc" ], [ 6, "desc" ], [ 7, "desc" ], [ 8, "desc" ], [ 9, "desc" ]]
+    });
     $('.dtBasicExample1').DataTable();
     $('.dataTables_length').addClass('bs-select');
 });
